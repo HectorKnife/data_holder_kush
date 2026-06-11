5a1043ce967fbb555ae393e5ba04e5f776c1c5c7
HectorKnife <138020494+HectorKnife@users.noreply.github.com>
2026-06-11 23:50:44 +0400

    Update main.py

--- main.py
+++ main.py
@@ -75,3 +75,10 @@ async def calculate(data: SimulationData):
     
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
+
+import os
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
